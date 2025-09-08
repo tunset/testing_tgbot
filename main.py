6e98925b70a87b5f009b282f24088da2b8149397
tunset <91646952+tunset@users.noreply.github.com>
2025-09-08 10:10:35 +0630

    main.py

--- main.py
+++ main.py
@@ -137,6 +137,7 @@ if __name__ == "__main__":
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
     print("✅ Attendance Bot is running...")
-    app.run_polling()
+    await app.run_polling()
 
     asyncio.run(main())
+
