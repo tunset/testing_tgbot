11c50c9b41af6d34005f0976580bedfe5d3220f9
tunset <91646952+tunset@users.noreply.github.com>
2025-11-23 17:59:02 +0630

    main.py

--- main.py
+++ main.py
@@ -121,8 +121,8 @@ if __name__ == "__main__":
         send_reminder,
         trigger="cron",
         day_of_week='mon-fri',
-        hour=17,
-        minute=55,
+        hour=18,
+        minute=05,
         args=[app]   # Pass app to the function
     )
     scheduler.start()
@@ -136,6 +136,7 @@ if __name__ == "__main__":
 
     print("✅ Bot + Web server started")
     app.run_polling()
+
 
 
 
